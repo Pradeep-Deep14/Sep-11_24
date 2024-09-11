531aa95a7fbd81ab98f77becfedbf4579039d43b
Pradeep <deeppradeep1998@gmail.com>
2024-09-11 10:00:32 +0530

    first commit

---
+++ 2.py
@@ -0,0 +1,20 @@
+#Nested to Flattened list without duplicates
+
+L=[1,1,[2,2,3,4],5,[6,7,8]]
+
+def nested_to_flattened_without_duplicates(L):
+    seen=set()
+    flattened=[]
+    for i in L:
+        if isinstance(i,list):
+            for item in i:
+                if item not in seen:
+                    flattened.append(item)
+                    seen.add(item)
+        else:
+                if i not in seen:
+                    flattened.append(i)
+                    seen.add(i)
+    return flattened
+
+print(nested_to_flattened_without_duplicates(L))
